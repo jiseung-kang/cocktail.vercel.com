414aebb5f426d70a4ea48911b907436651cdb6b6
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-06-15 15:44:41 +0900

    💄 favicon 추가, 로고 업데이트

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,14 +1,14 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import styled from 'styled-components';
 const Header = () => {
-  const handleOnStar = () => {
-    console.log('즐겨찾기 클릭');
-  };
-
   return (
     <HeaderContainer>
-      <div>로고</div>
-      <Link href={`/favorites`}>즐겨찾기</Link>
+      <Image src={'/cocktail.png'} width="30%" height="100%" />
+      <h1>My Cocktail</h1>
+      <Link href={`/favorites`}>
+        <a>⭐️</a>
+      </Link>
     </HeaderContainer>
   );
 };
@@ -20,8 +20,26 @@ const HeaderContainer = styled.div`
   width: 100%;
   top: 0;
   background-color: pink;
-  height: 50px;
+  height: 60px;
   z-index: 2;
+
+  h1 {
+    position: absolute;
+    width: 50%;
+    height: 100%;
+    top: 0.2em;
+    left: 25%;
+    margin: 0;
+    text-align: center;
+  }
+
+  a {
+    display: block;
+    position: absolute;
+    right: 0;
+    font-size: 2em;
+    padding: 10px;
+  }
 `;
 
 export default Header;
